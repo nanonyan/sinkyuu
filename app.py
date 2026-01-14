7e3f74cc38fa7be869a4a07714545288d56fd5a7
RoA10 <o223t029@st.kyoto-seika.ac.jp>
2026-01-14 17:31:31 +0900

    問題画面の調整

--- app.py
+++ app.py
@@ -1164,7 +1164,7 @@ def question():
                 q.question_text,
                 q.explanation,
                 c.name AS category_name,
-                ci.image_data
+                ci.image_url
             FROM quiz_questions q
             JOIN quiz_categories c
                 ON q.category_id = c.id
@@ -1219,6 +1219,17 @@ def question():
 
     correct_choice_id = next(c["id"] for c in choices if c["is_correct"])
 
+    image_url = question["image_url"]
+
+    if image_url:
+        image_url = (
+            image_url
+            .replace("\n", "")
+            .replace("\r", "")
+            .replace(" ", "")
+        )
+
+
     return render_template(
         "question.html",
         finished=False,
@@ -1228,7 +1239,8 @@ def question():
             "question_text": question["question_text"],
             "correct_choice_id": correct_choice_id,
             "explanation": question["explanation"],
-            "image_url": question["image_data"],
+            "image_url": question["image_url"],
+            "image_url": image_url,
         },
         choices=choices,
         genre=genre_en,
@@ -1244,23 +1256,6 @@ def debug_categories():
         cur.execute("SELECT id, name FROM quiz_categories;")
         rows = cur.fetchall()
     return str(rows)
-
-
-@app.route("/db-test")
-def db_test():
-    try:
-        conn = get_db()
-        cur = conn.cursor()
-        cur.execute("SELECT version();")
-        result = cur.fetchone()
-        cur.close()
-        conn.close()
-        if not result:
-            return "DB 接続成功！<br>(version取得に失敗しました)"
-        return f"DB 接続成功！<br>{result[0]}"
-    except Exception as e:
-        return f"DB 接続失敗 ❌<br>{e}"
-
 
 # =============================
 # 実行
